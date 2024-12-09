558516e0b893e00e26529aa0f701130afc009b98
csxqdxq <599585010@qq.com>
2024-12-09 21:49:21 +0800

    添加主界面

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -20,22 +20,22 @@ const info = reactive({
         <b>{{info.isLogin?'登录' : '注册'}}</b>
       </div>
       <div class="login-form">
-        <label class="children-row-center">
+        <span class="children-row-center">
           <input type="text" placeholder="用户名" class="white-input" v-model="info.username">
-        </label>
-        <label class="children-row-center">
+        </span>
+        <span class="children-row-center">
           <input type="password" placeholder="密码" class="white-input" v-model="info.password">
-        </label>
-        <label class="children-row-center" v-show="info.isLogin">
+        </span>
+        <span class="children-row-center" v-show="info.isLogin">
           <el-switch class="login-switch" v-model="info.remember" active-text="记住密码"/>
           <el-link class="login-forget" type="primary">忘记密码？</el-link>
-        </label>
-        <label class="children-row-center" v-show="!info.isLogin">
+        </span>
+        <span class="children-row-center" v-show="!info.isLogin">
           <input type="text" placeholder="邮箱" class="white-input" v-model="info.email">
-        </label>
-        <label class="children-row-center">
+        </span>
+        <span class="children-row-center">
           <button class="login-btn">{{info.isLogin?'登录':'注册'}}</button>
-        </label>
+        </span>
         <div class="children-row-center">
           <el-link type="primary" @click="info.isLogin=!info.isLogin">{{info.isLogin?'注册':'登录'}}</el-link>
         </div>
@@ -48,13 +48,22 @@ const info = reactive({
 .basemap {
   width: 100%;
   height: 100%;
-  background: brown;
+  background-image: url("@/assets/img/login_bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: left;
+  background-attachment: fixed;
   position: absolute;
   z-index: -1;
   top: 0;
   left: 0;
 }
-
+.login-box{
+ background-color: white;
+  padding: 30px 10px;
+  border-radius: 10px;
+  border: 1px solid gray;
+}
 .login-max-box {
   width: 100%;
   height: 100%;
@@ -70,9 +79,10 @@ const info = reactive({
   flex-direction: column;
   align-items: center;
 
+
 }
 
-.login-form > label {
+.login-form > span {
   padding: 5px;
   width: 100%;
   height: 30px;
@@ -98,8 +108,9 @@ const info = reactive({
 .login-btn {
   width: 180px;
   background-color: #933df9;
-  height: 25px;
+  height: 30px;
   border: none;
+  color: white;
   border-radius: 10px;
   cursor: pointer;
 }
